f1250fbb9b6f26010aa0b7218c384c6a7c6277e8
Colin <coliwogg@outlook.com>
2020-11-23 10:39:26 -0400

    Switched to using RAF and FC for reading/writing.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,21 +1,17 @@
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.RandomAccessFile;
+import java.io.*;
+import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        String fileLocation = "C:\\Users\\coliw\\OneDrive\\Documents\\University Year 4\\CS 4525\\Project 2\\course.csv";
+        String inputLocation = "C:\\Users\\coliw\\OneDrive\\Documents\\University Year 4\\CS 4525\\Project 2\\course.csv";
         String outputLocation = "C:\\Users\\coliw\\OneDrive\\Documents\\University Year 4\\CS 4525\\Project 2\\hfo.csv";
-        File in = new File(fileLocation);
+        File in = new File(inputLocation);
         File out = new File(outputLocation);
 
-        readFile(in, out);
+        createFile(in, out);
 
         Scanner scan = new Scanner(System.in);
         boolean begin = false;
@@ -31,78 +27,133 @@ public class Main {
                     scan.close();
                     System.exit(0);
                 } else if (query[0].equals("insert")) {
-                    insert(query);
+                    insert(query, out);
                     break;
                 } else if (query[0].equals("delete")) {
-                    delete(query);
+                    delete(query, out);
                     break;
                 } else if (query[0].equals("select")) {
-                    select(query);
+                    select(query, out);
                     break;
                 } else {
                     System.out.println(query[i]);
-                    break;
                 }
             }
             begin = false;
         }
     }
 
-    private static void delete(String[] query) {
+    private static void insert(String[] query, File file) throws FileNotFoundException {
+        //RandomAccessFile randomAccessFile = new RandomAccessFile(file, "rw");
+    }
+
+    private static void delete(String[] query, File file) {
 
     }
 
-    private static void insert(String[] query) {
+    private static void select(String[] query, File file) {
 
     }
 
-    private static void select(String[] query) {
+    private static void createFile(File in, File out) throws IOException {
+        RandomAccessFile inputFile = new RandomAccessFile(in, "r");
+        RandomAccessFile outputFile = new RandomAccessFile(out, "rw");
+        FileChannel inputChannel = inputFile.getChannel();
+        FileChannel outputChannel = outputFile.getChannel();
+        ByteBuffer inputBuffer = null;
+        ByteBuffer outputBuffer = null;
 
-    }
+        // Read attributes of input relation.
+        inputBuffer = ByteBuffer.allocate((int) inputChannel.size());
+        inputChannel.read(inputBuffer);
+        String fileContent = new String(inputBuffer.array(), StandardCharsets.UTF_8);
+        inputBuffer.flip();
+        inputChannel.close();
+        inputFile.close();
 
-    private static void readFile(File in, File out) throws IOException {
-        RandomAccessFile randomAccessFile = new RandomAccessFile(in, "rw");
-        FileChannel fileChannel = randomAccessFile.getChannel();
-        FileWriter writer = new FileWriter(out);
+        // Place attributes in string array.
+        String[] attributes = fileContent.split("\\r?\\n");
 
-        // Read column header for the table.
-        randomAccessFile.seek(3);
-        String line = randomAccessFile.readLine();
-        String[] header = line.split(",");
+        // Get header for attributes from array.
+        String[] header = attributes[0].split(",");
 
-        // Read column values into a list.
-        line = randomAccessFile.readLine();
+        // Add records to list.
         List<String[]> recordList = new ArrayList<>();
-        while(line != null) {
-            String[] record = line.split(",");
+        for(int i=1; i<attributes.length; i++) {
+            String[] record = attributes[i].split(",");
             recordList.add(record);
-            line = randomAccessFile.readLine();
         }
 
-        randomAccessFile.seek(0);
-
-        // Create empty string array for hash index.
+        // Create empty array for output.
         String[] output = new String[100];
         Arrays.fill(output, "");
 
-        // Format and save records to string array.
+        // Compute hash function on records and save at index.
         for(String[] record : recordList) {
             int hash = hashFunction(record[0]);
-            for (int i = 0; i < record.length; i++)
+            for(int i=0; i<record.length; i++) {
+                if(header[i].startsWith("\uFEFF"))
+                    header[i] = header[i].substring(1);
                 record[i] = header[i] + ": " + record[i];
+            }
             output[hash] += String.join(";", record) + ",";
         }
 
-        // Write record data to hash file organization.
-        for (int i=0; i<output.length; i++) {
-            writer.append(Integer.toString(i));
-            writer.append(",");
-            writer.append(output[i]);
-            writer.append("\n");
+        // Write to output file.
+        for(int i=0; i<output.length; i++) {
+            String line = Integer.toString(i) + "," + output[i] + "\n";
+            outputBuffer = ByteBuffer.wrap(line.getBytes("UTF-8"));
+            outputChannel.write(outputBuffer);
         }
-        writer.flush();
-        writer.close();
+        outputChannel.close();
+        outputFile.close();
     }
+
+//    private static void createFile(File in, File out) throws IOException {
+//        RandomAccessFile randomAccessFile = new RandomAccessFile(in, "r");
+//        FileChannel fileChannel = randomAccessFile.getChannel();
+//        FileWriter writer = new FileWriter(out);
+//
+//        // Read column header for the table.
+//        randomAccessFile.seek(0);
+//        String line = randomAccessFile.readLine();
+//        String[] header = line.split(",");
+//
+//        // Read column values into a list.
+//        line = randomAccessFile.readLine();
+//        List<String[]> recordList = new ArrayList<>();
+//        while(line != null) {
+//            String[] record = line.split(",");
+//            recordList.add(record);
+//            line = randomAccessFile.readLine();
+//        }
+//
+//        randomAccessFile.seek(0);
+//
+//        // Create empty string array for hash index.
+//        String[] output = new String[100];
+//        Arrays.fill(output, "");
+//
+//        // Format and save records to string array.
+//        for(String[] record : recordList) {
+//            int hash = hashFunction(record[0]);
+//            for (int i = 0; i < record.length; i++) {
+//                record[i] = header[i] + ": " + record[i];
+//            }
+//            output[hash] += String.join(";", record) + ",";
+//        }
+//
+//        // Write record data to hash file organization.
+//        for (int i=0; i<output.length; i++) {
+//            writer.append(Integer.toString(i));
+//            writer.append(",");
+//            writer.append(output[i]);
+//            writer.append("\n");
+//        }
+//        writer.flush();
+//        writer.close();
+//        randomAccessFile.close();
+//    }
 
     private static int hashFunction(String key) {
         return Math.abs(key.hashCode() % 100);
